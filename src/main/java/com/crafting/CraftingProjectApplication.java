75ceccd9722fd39c6e61faa366c9430c8ba5ec3b
SanteriSil <silkkueune@gmail.com>
2025-12-14 00:37:22 +0200

    Add initial Spring Boot application setup with Gradle configuration and necessary files

---
+++ src/main/java/com/crafting/CraftingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.crafting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crafting/CraftingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
