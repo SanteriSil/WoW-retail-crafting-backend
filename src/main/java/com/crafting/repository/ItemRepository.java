51bfd4a68bba5889027247038ccd27a1a826d1a0
SanteriSil <silkkueune@gmail.com>
2026-02-07 19:18:03 +0200

    refactor: simplify ItemRepository and RecipeRepository interfaces

--- src/main/java/com/crafting/repository/ItemRepository.java
+++ src/main/java/com/crafting/repository/ItemRepository.java
@@ -3,7 +3,10 @@ package com.crafting.repository;
 import com.crafting.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
+public interface ItemRepository extends JpaRepository<Item, Long>{
+    List<Item> findAll();
+    List<Item> findAllById(Iterable<Long> id);
 }
