32777275b4605cd10bf85f64f42640c9b3e635ce
SanteriSil <silkkueune@gmail.com>
2026-02-09 19:06:03 +0200

    fix: add missing import for Query annotation in ItemRepository

--- src/main/java/com/crafting/repository/ItemRepository.java
+++ src/main/java/com/crafting/repository/ItemRepository.java
@@ -4,6 +4,7 @@ import com.crafting.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long>{
